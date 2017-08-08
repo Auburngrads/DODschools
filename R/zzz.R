f75076f6ba28645415324ad0496f8ba56edca10a
Auburngrads <auburngrads@live.com>
2017-08-08 17:22:49 -0400

    Update R/zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   
   .afit_thesis_bib <<- 'inst/rmarkdown/templates/afit_thesis/skeleton/references/my_bib.bib'
-
+  .afit_thesis_template <<- system.file('rmarkdown',
+                                        'templates',
+                                        'afit_thesis',
+                                        'resources',
+                                        'templateb.tex',
+                                        package = 'DODschools')
   }
